ec3f703430d43380469cc6a85c35652f46e3bdd7
Mohit Bagra <mbagra@microsoft.com>
2018-05-15 19:55:47 -0400

    context

--- src/Library/Utilities/Context.ts
+++ src/Library/Utilities/Context.ts
@@ -1,51 +1,79 @@
-import { IObservable, Observable } from "VSSUI/Utilities/Observable";
+export interface IObservable<T> {
+    subscribe(observer: (value: T, action?: string) => void, action?: string): void;
+    unsubscribe(observer: (value: T, action?: string) => void, action?: string): void;
+}
 
-/**
- * When an action occurs on an IObservableCollection the event should take the form
- * of an ICollectionEntry<T> where T is the type of object being stored in
- * the collection.
- */
+interface IObservableEvent<T> {
+    action: string;
+    value: T;
+}
+
+export class Observable<T> implements IObservable<T> {
+    private observers: { [action: string]: ((value: T, action: string) => void)[] } = {};
+    private events?: IObservableEvent<T>[];
+
+    public notify(value: T, action: string, persistEvent?: boolean): void {
+        if (this.observers[action]) {
+            const observers = this.observers[action].slice();
+            for (const observer of observers) {
+                observer(value, action);
+            }
+        }
+
+        if (this.observers[""]) {
+            const observers = this.observers[""].slice();
+            for (const observer of observers) {
+                observer(value, action);
+            }
+        }
+
+        if (persistEvent) {
+            if (!this.events) {
+                this.events = [];
+            }
+
+            this.events.push({ action: action, value: value });
+        }
+    }
+
+    public subscribe(observer: (value: T, action: string) => void, action?: string): void {
+        const innerAction = action || "";
+        if (!this.observers[innerAction]) {
+            this.observers[innerAction] = [];
+        }
+
+        this.observers[innerAction].push(observer);
+
+        // Fire the callback for any events that were persisted when they were sent.
+        if (this.events) {
+            for (const event of this.events) {
+                this.notify(event.value, event.action, false);
+            }
+        }
+    }
+
+    public unsubscribe(observer: (value: T, action: string) => void, action?: string): void {
+        const innerAction = action || "";
+        if (this.observers[innerAction]) {
+            const observerIndex = this.observers[innerAction].indexOf(observer);
+            if (observerIndex >= 0) {
+                this.observers[innerAction].splice(observerIndex, 1);
+            }
+        }
+    }
+}
+
 export interface ICollectionEntry<T> {
     key: string;
     value?: T;
 }
 
-/**
- * An Observable collection is used to track a set of objects by name and offer notifications
- * for consumers when the collection has changed.
- *
- * EventTypes:
- *  add - ICollectionEvent<V>
- */
 export interface IObservableCollection<V> extends IObservable<ICollectionEntry<V>> {
-
-    /**
-     * Adding an object to the collection will notify all observers of the collection
-     * and keep track of the objects.
-     *
-     * @param objectName - name of the object be registered.
-     *
-     * @param objectDefinition - details of the object being registered
-     */
     add(objectName: string, objectDefinition: V): void;
-
-    /**
-     * get is used to retrieve the objectDefinition for named object.
-     *
-     * @param objectName - name of the object to get the definition.
-     */
     get(objectName: string): V | undefined;
-
-    /**
-     * A read-only collection of the existing objects.
-     */
     keys(): string[];
 }
 
-/**
- * An ObservableCollection can be used to key a named collection of objects
- * and offer an observable endpoint.
- */
 export class ObservableCollection<V> extends Observable<ICollectionEntry<V>> implements IObservableCollection<V> {
     private objects: { [objectName: string]: V } = {};
 
@@ -65,40 +93,27 @@ export class ObservableCollection<V> extends Observable<ICollectionEntry<V>> imp
     }
 }
 
-export interface IAppService {
-    serviceStart?(pageContext: IAppContext): void;
-    serviceEnd?(pageContext: IAppContext): void;
+export interface IVssService {
+    serviceStart?(pageContext: IVssPageContext): void;
+    serviceEnd?(pageContext: IVssPageContext): void;
 }
 
-export abstract class AppService implements IAppService {
-    protected pageContext: IAppContext;
+export abstract class VssService implements IVssService {
+    protected pageContext: IVssPageContext;
 
-    public serviceStart(pageContext: IAppContext): void {
+    public serviceStart(pageContext: IVssPageContext): void {
         this.pageContext = pageContext;
     }
 
-    public serviceEnd(_pageContext: IAppContext): void {
+    public serviceEnd(_pageContext: IVssPageContext): void {
         // dispose
     }
-
-    public serviceRestart(pageContext: IAppContext): void {
-        this.pageContext = pageContext;
-    }
 }
 
-/**
- * An IAppObservableService<T> is a standard service that offers an observable as part
- * of the service itself. If the service wants to publish events this is an easy
- * model to follow.
- */
-export interface IAppObservableService<T> extends IAppService, IObservable<T> {
+export interface IVssObservableService<T> extends IVssService, IObservable<T> {
 }
 
-/**
- * An ObservableService is a service that allows callers to signup for notifications when
- * data within the service is changed.
- */
-export abstract class AppObservableService<T> extends AppService implements IAppObservableService<T> {
+export abstract class VssObservableService<T> extends VssService implements IVssObservableService<T> {
     private observable = new Observable<T>();
 
     public subscribe(observer: (value: T, action: string) => void, action?: string): void {
@@ -114,41 +129,29 @@ export abstract class AppObservableService<T> extends AppService implements IApp
     }
 }
 
-/**
- * A service factory is the method used to create and initialize an IAppService.
- */
-export type ServiceFactory = new () => IAppService;
+export type ServiceFactory = new () => IVssService;
 
-/**
- * Details that describe how a service is created and its capabilities.
- */
 export interface IServiceDefinition {
-    /**
-     * This is the constructor used to create an instance of the service.
-     */
     serviceFactory: ServiceFactory;
 }
 
-/**
- * Create and expose the service registry to the platform for developers to register
- * their services.
- */
 export const Services: IObservableCollection<IServiceDefinition> = new ObservableCollection<IServiceDefinition>();
 
-export interface IAppContext {
-    getService<T extends IAppService>(serviceName: string): T;
+export interface IVssPageContext {
+    getService<T extends IVssService>(serviceName: string): T;
 }
 
-export class AppContext implements IAppContext {
+class VssPageContext implements IVssPageContext {
     private initializatonInProgress: { [serviceName: string]: boolean } = {};
-    private serviceInstances: { [serviceName: string]: IAppService } = {};
+
+    private serviceInstances: { [serviceName: string]: IVssService } = {};
     private services: IObservableCollection<IServiceDefinition>;
 
     constructor(serviceRegistry: IObservableCollection<IServiceDefinition>) {
         this.services = serviceRegistry;
     }
 
-    public getService<T extends IAppService>(serviceName: string): T {
+    public getService<T extends IVssService>(serviceName: string): T {
         let registeredService = this.serviceInstances[serviceName];
 
         // If no service is available, we will start an instance of the service.
@@ -167,15 +170,14 @@ export class AppContext implements IAppContext {
     }
 
     private _loadService<T>(serviceName: string, serviceDefinition: IServiceDefinition): T {
-        let registeredService: IAppService;
+        let registeredService: IVssService;
 
         if (this.initializatonInProgress[serviceName]) {
             throw `Unable to initialize service due to cyclic dependency: ${serviceName}`;
         }
 
         try {
-
-            // Mark this service as initialization in progress to detect cyclic dependencies in serviceStart.
+            // Mark this service as initialization in progress to detect cyclic dependencies in _serviceStart.
             this.initializatonInProgress[serviceName] = true;
 
             // Create and initialize a new instance of the service.
@@ -196,3 +198,8 @@ export class AppContext implements IAppContext {
         return registeredService as T;
     }
 }
+
+/**
+ * Context for the currently executing page
+ */
+export const PageContext: IVssPageContext = new VssPageContext(Services);
